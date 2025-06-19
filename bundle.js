7956212abc987966a57227cdeada68d1eb95d01d
make5015 <make1396@gmail.com>
2025-06-19 20:06:05 +0900

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,15 +1,13 @@
 (async () => {
-  const initRembg = (await import('./16a06d9fccef7819a504.mjs')).default;
-
-  const rembg = await initRembg({
+  const rembg = await (await import('./16a06d9fccef7819a504.mjs')).default({
     locateFile: (path) => {
       if (path.endsWith('.wasm')) return './03bafc41e28c2f1e46f4.wasm';
       return path;
-    },
+    }
   });
 
   const ort = await import('./vendors-node_modules_onnxruntime-web_dist_ort_bundle_min_mjs.bundle.js');
-  const session = await rembg.createSession(ort);
 
+  const session = await rembg.createSession(ort);
   window.removeBackground = async (buffer) => await session.remove(buffer);
 })();
