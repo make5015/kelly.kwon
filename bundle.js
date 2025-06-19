bf881edc313d6701090cb718f596bd9650ae7267
make5015 <make1396@gmail.com>
2025-06-19 19:31:01 +0900

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,26 +1,13 @@
-import initRembg from './16a06d9fccef7819a504.mjs';
-import * as ort from './vendors-node_modules_onnxruntime-web_dist_ort_bundle_min_mjs.bundle.js';
-
-let session = null;
-
-async function loadModel() {
-  const rembg = await initRembg({
-    locateFile(path) {
-      // .wasm 파일 로딩 위치 지정
-      if (path.endsWith('.wasm')) {
-        return './03bafc41e28c2f1e46f4.wasm';
-      }
+(async () => {
+  const rembg = await (await import('./16a06d9fccef7819a504.mjs')).default({
+    locateFile: (path) => {
+      if (path.endsWith('.wasm')) return './03bafc41e28c2f1e46f4.wasm';
       return path;
-    },
+    }
   });
 
-  session = await rembg.createSession(ort);
-}
+  const ort = await import('./vendors-node_modules_onnxruntime-web_dist_ort_bundle_min_mjs.bundle.js');
 
-async function removeBackground(buffer) {
-  if (!session) await loadModel();
-  return await session.remove(buffer);
-}
-
-// 전역 등록
-window.removeBackground = removeBackground;
+  const session = await rembg.createSession(ort);
+  window.removeBackground = async (buffer) => await session.remove(buffer);
+})();
